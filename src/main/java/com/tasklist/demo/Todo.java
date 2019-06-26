aa4f341414a03621e462fd4cca6245378f361e1f
jasinsky187 <razziel496@gmail.com>
2019-06-26 12:30:36 +0200

    Todo model created

---
+++ src/main/java/com/tasklist/demo/Todo.java
@@ -0,0 +1,24 @@
+package com.tasklist.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "completed", nullable = false)
+    private boolean completed;
+}
